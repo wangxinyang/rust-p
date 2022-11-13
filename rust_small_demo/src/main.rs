158779c9b8102522e59a2f795606e3e03f13f5af
Tosei <359387050@qq.com>
2022-11-13 19:42:08 +0900

    feat: update examples

--- rust_small_demo/src/main.rs
+++ rust_small_demo/src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+fn main() {
+    let test = Test {};
+    println!("{}", test.test());
+    println!("{}", test.test2());
+}
+
+struct Test;
+
+impl Test {
+    fn test(&self) -> &str {
+        "hello world"
+    }
+
+    fn test2<'a>(self) -> &'a str {
+        "tosei"
+    }
+}
