cccba55349549f16b3cf30c36da5c6d9b135bdbd
Tosei <359387050@qq.com>
2022-11-14 22:31:08 +0900

    feat: update examples

--- rust_small_demo/src/main.rs
+++ rust_small_demo/src/main.rs
@@ -3,4 +3,10 @@ fn main() {
     let y = String::from("aaaa") + "tosei";
     println!("{}", x);
     println!("{}", y);
+
+    let a = 10;
+    let b = &a as *const i32;
+    println!("{}", &a); // 10
+    println!("{:p}", &a); // 0x7ff7be348d04
+    println!("{:?}", b); // 0x7ff7be348d04
 }
