57e46cee8818b3263ff992b6345002e46cfc450b
Tosei <359387050@qq.com>
2022-11-14 21:41:17 +0900

    feat: update examples

--- rust_small_demo/src/main.rs
+++ rust_small_demo/src/main.rs
@@ -1,17 +1,25 @@
-fn main() {
-    let test = Test {};
-    println!("{}", test.test());
-    println!("{}", test.test2());
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
 }
 
-struct Test;
+impl<T> Deref for MyBox<T> {
+    type Target = T;
 
-impl Test {
-    fn test(&self) -> &str {
-        "hello world"
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
+}
 
-    fn test2<'a>(self) -> &'a str {
-        "tosei"
-    }
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
