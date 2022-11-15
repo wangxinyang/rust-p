ee3b8cf868c9dfa5478a188aeed864899334dcfa
Tosei <359387050@qq.com>
2022-11-15 17:25:15 +0900

    feat: add some examples

--- rust_small_demo/src/main.rs
+++ rust_small_demo/src/main.rs
@@ -1,1 +1,50 @@
-fn main() {}
+use tokio::sync::{Semaphore, SemaphorePermit};
+
+struct Museum {
+    ticket: Semaphore,
+}
+
+#[allow(dead_code)]
+struct Ticket<'a> {
+    permit: SemaphorePermit<'a>,
+}
+
+impl<'a> Drop for Ticket<'a> {
+    fn drop(&mut self) {
+        println!("Ticket freed");
+    }
+}
+
+impl Museum {
+    fn new(count: usize) -> Self {
+        Self {
+            ticket: Semaphore::new(count),
+        }
+    }
+
+    fn get_ticket(&self) -> Option<Ticket<'_>> {
+        match self.ticket.try_acquire() {
+            Ok(permit) => Some(Ticket { permit }),
+            Err(_) => None,
+        }
+    }
+
+    fn ticket(&self) -> usize {
+        self.ticket.available_permits()
+    }
+}
+
+fn main() {
+    let museum = Museum::new(50);
+    let _ticket = museum.get_ticket().unwrap();
+    assert_eq!(49, museum.ticket());
+
+    let _tickets: Vec<Ticket> = (0..49).map(|_| museum.get_ticket().unwrap()).collect();
+    assert_eq!(0, museum.ticket());
+    {
+        assert!(museum.get_ticket().is_none());
+        drop(_ticket);
+        assert_eq!(1, museum.ticket());
+    }
+    println!("-------");
+}
