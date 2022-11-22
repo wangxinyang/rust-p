eb37f992bcd4647f6e25d5152621577744744a35
Tosei <359387050@qq.com>
2022-11-22 10:47:04 +0900

    feat: add transfer test

---
+++ rust_small_demo/src/main-implcopy-transfer.rs
@@ -0,0 +1,58 @@
+fn main() {
+    let str1 = "tosei";
+    // because impl ToOwned for str
+    // Owned = String
+    let str2 = str1.to_owned();
+    print_type_of(&str2);
+    println!("str2 is: {:?}", str2);
+
+    // &str -> String
+    let str = str1.to_string();
+    print_type_of(&str);
+    println!("str is: {:?}", str);
+
+    // &str -> bytes slice
+    let str1_bytes = str1.as_bytes();
+    print_type_of(&str1_bytes);
+    println!("str1_bytes is: {:?}", str1_bytes);
+
+    // bytes slice -> bytes array
+    // let length = str1_bytes.len();
+    // attempt to use a non-constant value in a constant non-constant value
+    // let str1_num: [u8; length] = str1_bytes.try_into().unwrap();
+    // array length must be the same as the slice length
+    let str1_num: [u8; 5] = str1_bytes.try_into().unwrap();
+    print_type_of(&str1_num);
+    println!("str1_num is: {:?}", str1_num);
+
+    // bytes slice -> Vec
+    // because impl ToOwned for [T], Owned = Vec<T>
+    let vec_from_slice = str1_bytes.to_owned();
+    print_type_of(&vec_from_slice);
+    println!("vec_from_slice is {:?}", vec_from_slice);
+
+    // bytes slice -> Vec
+    let str1_vec = str1_bytes.to_vec();
+    print_type_of(&str1_vec);
+    println!("str1_vec is {:?}", str1_vec);
+
+    // Vec -> String
+    let str_from_vec = String::from_utf8(str1_vec).unwrap();
+    print_type_of(&str_from_vec);
+    println!("str_from_vec is {:?}", str_from_vec);
+
+    // String -> Vec
+    let vec_from_string = str_from_vec.into_bytes();
+    print_type_of(&vec_from_string);
+    println!("vec_from_string is {:?}", vec_from_string);
+
+    // bytes slice -> u32
+    let bytes_arr: [u8; 4] = [12, 32, 44, 55];
+    let bytes_u32 = u32::from_be_bytes(bytes_arr);
+    print_type_of(&bytes_u32);
+    println!("bytes_u32 is {:?}", bytes_u32);
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("type is = {}", std::any::type_name::<T>())
+}
--- rust_small_demo/src/main.rs
+++ rust_small_demo/src/main.rs
@@ -1,58 +1,41 @@
-fn main() {
-    let str1 = "tosei";
-    // because impl ToOwned for str
-    // Owned = String
-    let str2 = str1.to_owned();
-    print_type_of(&str2);
-    println!("str2 is: {:?}", str2);
+use std::rc::Rc;
 
-    // &str -> String
-    let str = str1.to_string();
-    print_type_of(&str);
-    println!("str is: {:?}", str);
-
-    // &str -> bytes slice
-    let str1_bytes = str1.as_bytes();
-    print_type_of(&str1_bytes);
-    println!("str1_bytes is: {:?}", str1_bytes);
-
-    // bytes slice -> bytes array
-    // let length = str1_bytes.len();
-    // attempt to use a non-constant value in a constant non-constant value
-    // let str1_num: [u8; length] = str1_bytes.try_into().unwrap();
-    // array length must be the same as the slice length
-    let str1_num: [u8; 5] = str1_bytes.try_into().unwrap();
-    print_type_of(&str1_num);
-    println!("str1_num is: {:?}", str1_num);
-
-    // bytes slice -> Vec
-    // because impl ToOwned for [T], Owned = Vec<T>
-    let vec_from_slice = str1_bytes.to_owned();
-    print_type_of(&vec_from_slice);
-    println!("vec_from_slice is {:?}", vec_from_slice);
-
-    // bytes slice -> Vec
-    let str1_vec = str1_bytes.to_vec();
-    print_type_of(&str1_vec);
-    println!("str1_vec is {:?}", str1_vec);
-
-    // Vec -> String
-    let str_from_vec = String::from_utf8(str1_vec).unwrap();
-    print_type_of(&str_from_vec);
-    println!("str_from_vec is {:?}", str_from_vec);
-
-    // String -> Vec
-    let vec_from_string = str_from_vec.into_bytes();
-    print_type_of(&vec_from_string);
-    println!("vec_from_string is {:?}", vec_from_string);
-
-    // bytes slice -> u32
-    let bytes_arr: [u8; 4] = [12, 32, 44, 55];
-    let bytes_u32 = u32::from_be_bytes(bytes_arr);
-    print_type_of(&bytes_u32);
-    println!("bytes_u32 is {:?}", bytes_u32);
+#[derive(Debug)]
+struct Transfer {
+    trans: Rc<Vec<u8>>,
+    message: String,
 }
 
-fn print_type_of<T>(_: &T) {
-    println!("type is = {}", std::any::type_name::<T>())
+impl Transfer {
+    fn new(trans: &[u8], message: &str) -> Self {
+        Transfer {
+            trans: Rc::new(trans.to_owned()),
+            message: message.to_owned(),
+        }
+    }
+
+    fn trans(&self) -> Vec<u8> {
+        self.trans.clone().to_vec()
+    }
+
+    fn message(&self) -> String {
+        self.message.clone()
+    }
+}
+
+fn main() {
+    let transfer = Transfer::new(b"tosei", "hello");
+    let trans = transfer.trans();
+    let message = transfer.message();
+    // Vec -> [u8]
+    let trans_array: [u8; 5] = trans.try_into().unwrap();
+    println!("trans_array is: {:?}", trans_array);
+
+    // String  -> [u8]
+    let message_array: [u8; 5] = message.as_bytes().try_into().unwrap();
+    println!("message_array is: {:?}", message_array);
+
+    // [u8] -> &str
+    let trans_str = std::str::from_utf8(&trans_array).unwrap();
+    println!("trans_str is: {:?}", trans_str);
 }
