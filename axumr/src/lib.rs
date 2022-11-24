9a79139f9046a6aa6580580815e8353f36644276
Tosei <359387050@qq.com>
2022-11-24 13:04:03 +0900

    feat: add stream&sink example

--- axumr/src/lib.rs
+++ axumr/src/lib.rs
@@ -17,39 +17,23 @@ pub async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
 async fn handle_socket(mut socket: WebSocket) {
     if let Some(msg) = socket.recv().await {
         if let Ok(msg) = msg {
-            match msg {
-                Message::Text(t) => {
-                    println!("client sent str: {:?}", t);
-                }
-                Message::Binary(_) => {
-                    println!("client sent binary data");
-                }
-                Message::Ping(_) => {
-                    println!("socket ping");
-                }
-                Message::Pong(_) => {
-                    println!("socket pong");
-                }
-                Message::Close(_) => {
-                    println!("client disconnected");
-                    return;
-                }
+            if let Message::Text(msg) = msg {
+                println!("client sent str: {:?}", msg);
             }
         } else {
             println!("client disconnected");
-            return;
         }
     }
 
-    loop {
-        if socket
-            .send(Message::Text(String::from("Hi!")))
-            .await
-            .is_err()
-        {
-            println!("client disconnected");
-            return;
-        }
-        tokio::time::sleep(std::time::Duration::from_secs(3)).await;
-    }
+    // loop {
+    //     if socket
+    //         .send(Message::Text(String::from("Hi!")))
+    //         .await
+    //         .is_err()
+    //     {
+    //         println!("client disconnected");
+    //         return;
+    //     }
+    //     tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+    // }
 }
--- rust_small_demo/practice_coding/small_demo/src/main.rs
+++ rust_small_demo/practice_coding/small_demo/src/main.rs
@@ -1,3 +1,68 @@
-fn main() {
-    println!("Hello, world!");
+use bytes::BytesMut;
+use futures::{SinkExt, StreamExt};
+use std::io;
+use tokio::net::TcpListener;
+use tokio_util::codec::{Decoder, Encoder};
+
+#[derive(Debug)]
+pub enum ConnectionError {
+    Io(io::Error),
+    Disconnected,
+}
+
+impl From<io::Error> for ConnectionError {
+    fn from(err: io::Error) -> Self {
+        ConnectionError::Io(err)
+    }
+}
+
+type Message = String;
+
+struct EchoCodec;
+
+impl Encoder<Message> for EchoCodec {
+    type Error = ConnectionError;
+
+    fn encode(&mut self, item: Message, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        dst.extend_from_slice(item.as_bytes());
+        Ok(())
+    }
+}
+
+impl Decoder for EchoCodec {
+    type Item = Message;
+
+    type Error = ConnectionError;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        if src.is_empty() {
+            return Ok(None);
+        }
+        let data = src.split_to(src.len());
+        Ok(Some(String::from_utf8_lossy(&data[..]).into_owned()))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "0.0.0.0:3300";
+    let listener = TcpListener::bind(addr).await?;
+    println!("listen on {:?}", addr);
+
+    loop {
+        let (stream, addr) = listener.accept().await?;
+        println!("accepted connection from: {}", addr);
+
+        tokio::spawn(async move {
+            let codec = EchoCodec {};
+            let mut framed = codec.framed(stream);
+            while let Some(message) = framed.next().await {
+                if let Ok(message) = message {
+                    println!("received message is: {:?}", message);
+                    framed.send(message).await?;
+                }
+            }
+            Ok::<(), ConnectionError>(())
+        });
+    }
 }
