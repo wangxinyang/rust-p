53f2170432ea51af0c49d5171483a0d11c8213e6
Tosei <359387050@qq.com>
2022-11-24 13:50:34 +0900

    feat: coding the kv

--- rust_small_demo/practice_coding/kv/src/client.rs
+++ rust_small_demo/practice_coding/kv/src/client.rs
@@ -1,13 +1,31 @@
 mod pb;
 
-use pb::*;
-use prost::Message;
+use crate::pb::{Request, Response};
+use anyhow::{Ok, Result};
+use futures::{SinkExt, StreamExt};
+use tokio::net::TcpStream;
+use tokio_util::codec::LengthDelimitedCodec;
 
-fn main() {
-    let request = RequestGet {
-        key: "key".to_string(),
-    };
-    let mut buf = Vec::new();
-    request.encode(&mut buf).unwrap();
-    println!("{:?}", buf);
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
+    let addr = "localhost:3001";
+    let client = TcpStream::connect(addr).await?;
+    let mut stream = LengthDelimitedCodec::builder()
+        .length_field_length(2)
+        .new_framed(client);
+    let request = Request::new_get("hello".to_string());
+    stream.send(request.into()).await?;
+
+    let request = Request::new_put("hello".to_string(), "world".to_string());
+    stream.send(request.into()).await?;
+
+    while let Some(std::result::Result::Ok(buf)) = stream.next().await {
+        let msg = Response::try_from(buf)?;
+        println!("Got msg is: {:?}", msg);
+    }
+
+    Ok(())
 }
--- rust_small_demo/practice_coding/kv/src/pb/mod.rs
+++ rust_small_demo/practice_coding/kv/src/pb/mod.rs
@@ -1,8 +1,23 @@
 mod abi;
 
+use self::request::Command;
 pub use abi::*;
 use bytes::{Bytes, BytesMut};
 use prost::Message;
+
+impl Request {
+    pub fn new_get(key: String) -> Self {
+        Self {
+            command: Some(Command::Get(RequestGet { key })),
+        }
+    }
+
+    pub fn new_put(key: String, value: String) -> Self {
+        Self {
+            command: Some(Command::Put(RequestPut { key, value })),
+        }
+    }
+}
 
 impl Response {
     pub fn new(key: String, value: String) -> Self {
@@ -27,6 +42,22 @@ impl TryFrom<BytesMut> for Request {
 
     fn try_from(value: BytesMut) -> Result<Self, Self::Error> {
         Message::decode(value)
+    }
+}
+
+impl TryFrom<BytesMut> for Response {
+    type Error = prost::DecodeError;
+
+    fn try_from(value: BytesMut) -> Result<Self, Self::Error> {
+        Message::decode(value)
+    }
+}
+
+impl From<Request> for Bytes {
+    fn from(value: Request) -> Self {
+        let mut buf = BytesMut::with_capacity(value.encoded_len());
+        value.encode(&mut buf).unwrap();
+        buf.freeze()
     }
 }
 
