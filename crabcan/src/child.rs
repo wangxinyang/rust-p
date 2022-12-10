863f89afe7a7ad60e6ff45cff5df0c7f416d6682
Tosei <359387050@qq.com>
2022-12-10 10:43:31 +0900

    refactor: update file

--- crabcan/src/child.rs
+++ crabcan/src/child.rs
@@ -1,5 +1,5 @@
 use crate::{config::ContainerOpts, errors::Errcode};
-use nix::sched::{clone, CloneFlags};
+// use nix::sched::{clone, CloneFlags};
 use nix::sys::signal::Signal;
 use nix::unistd::Pid;
 use tracing::info;
@@ -15,16 +15,17 @@ fn child(config: ContainerOpts) -> usize {
 
 const STACK_SIZE: usize = 1024 * 1024;
 
-pub fn generate_child_process(config: ContainerOpts) -> Result<Pid, Errcode> {
-    let mut tmp_stack: [u8; STACK_SIZE] = [0; STACK_SIZE];
-    let mut flags = CloneFlags::empty();
-    match clone(
-        Box::new(|| child(config)),
-        &mut tmp_stack,
-        flags,
-        Some(Signal::SIGCHLD as i32),
-    ) {
-        Ok(pid) => Ok(pid),
-        Err(e) => Err(Errcode::ChildProcessError(0)),
-    }
-}
+// pub fn generate_child_process(config: ContainerOpts) -> Result<Pid, Errcode> {
+//     let mut tmp_stack: [u8; STACK_SIZE] = [0; STACK_SIZE];
+//     let mut flags = CloneFlags::empty();
+//     let mut flags = CloneFlags::empty();
+//     match clone(
+//         Box::new(|| child(config)),
+//         &mut tmp_stack,
+//         flags,
+//         Some(Signal::SIGCHLD as i32),
+//     ) {
+//         Ok(pid) => Ok(pid),
+//         Err(e) => Err(Errcode::ChildProcessError(0)),
+//     }
+// }
